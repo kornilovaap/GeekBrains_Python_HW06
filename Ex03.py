e0b111475a1961b8a7d550aba4e3d4e5f064feb6
kornilovaap <67694230+kornilovaap@users.noreply.github.com>
2020-09-03 16:35:59 +0300

    Add files via upload

---
+++ Ex03.py
@@ -0,0 +1,36 @@
+'''
+Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность), income (доход).
+Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы: оклад и премия.
+Hапример, {"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+'''
+
+
+class Worker:
+    name = None
+    surname = None
+    position = None
+    income = None
+
+    def __init__(self, name, surname, position, income):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.income = income
+
+
+class Position (Worker):
+    def __init__(self, name, surname, position, income):
+        super().__init__(name, surname, position, income)
+
+    def get_full_name(self):
+        return self.name + self.surname
+
+    def get_total_income(self):
+        return self.income
+
+
+manager = Position("Анна ", "Ановна", 'manager', 1000)
+print(manager.get_full_name(), manager.get_total_income())
